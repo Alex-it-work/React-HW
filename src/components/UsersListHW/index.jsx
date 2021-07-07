37f14540c196e476cf080643a71ac52288ffe258
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-07-07 21:17:46 +0300

    Add UL mapUser, onClickHandler,  render

--- src/components/UsersListHW/index.jsx
+++ src/components/UsersListHW/index.jsx
@@ -9,17 +9,81 @@ const dbUsers = [
     age: 33,
     imgSrc: "http://vertex-art.ru/wp-content/uploads/portretvizo_.jpg",
   },
+  {
+    id: 2,
+    firstName: "Den",
+    secondName: "Polo",
+    age: 44,
+    imgSrc:
+      "https://st.depositphotos.com/1008939/1880/i/950/depositphotos_18807295-stock-photo-portrait-of-handsome-man.jpg",
+  },
+  {
+    id: 3,
+    firstName: "Martin",
+    secondName: "King",
+    age: 55,
+    imgSrc:
+      "https://upload.wikimedia.org/wikipedia/commons/0/05/Martin_Luther_King%2C_Jr..jpg",
+  },
+  {
+    id: 4,
+    firstName: "John",
+    secondName: "Lennon",
+    age: 22,
+    imgSrc:
+      "https://cdn.ananasposter.ru/image/cache/catalog/poster/music/85/8817-1000x830.jpg",
+  },
+  {
+    id: 5,
+    firstName: "Angela",
+    secondName: "Merkel",
+    age: 18,
+    imgSrc:
+      "hhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYni8G4eLkns7X1Ku10jX2jtL3gGDF5drU2A&usqp=CAU",
+  },
+  {
+    id: 6,
+    firstName: "John",
+    secondName: "Deer",
+    age: 33,
+    imgSrc: "http://vertex-art.ru/wp-content/uploads/portretvizo_.jpg",
+  },
+  {
+    id: 7,
+    firstName: "Silvestre",
+    secondName: "Stallone",
+    age: 25,
+    imgSrc:
+      "https://i.pinimg.com/originals/45/c8/63/45c863ced621cfb5cc6ca6162cd48c9a.jpg",
+  },
 ];
 
 class UserListHW extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      users: dbUsers.map(u => ({ ...u, isSelected: false })),
+    };
   }
-
+  mapUser = (user, index) => {
+    const { users } = this.state;
+    const onClickHandler = () => {
+      const newUsers = [...users];
+      newUsers[index].isSelected = !newUsers[index].isSelected;
+      this.setState({ users: newUsers });
+    };
+    return (
+      <UserListItemHW
+        key={user.id}
+        user={user}
+        onClickHandler={onClickHandler}
+      />
+    );
+  };
   render() {
-    return <div></div>;
+    const users = this.state;
+    return <ul>{users.map(this.mapUser)}</ul>;
   }
 }
 
